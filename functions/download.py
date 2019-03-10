c1f8a92bfa9a7cdf0e58108bb3d259717ef546db
sungsalex <cloudallera@gmail.com>
2019-03-11 00:40:43 +0900

    putSubmit() 메서드 삭제
    Signed-off-by: sungsalex <cloudallera@gmail.com>

--- functions/download.py
+++ functions/download.py
@@ -84,36 +84,3 @@ def postDownload(url,
         print("UnexpectedError:{}".format(e))
 
     return resp
-
-
-# put Method 방식으로 웹 컨텐츠를 수정하는 함수
-def putSubmit(url, data=None, cookie=None, headers=header,
-              retries=base_retries):
-    '''
-    이 함수는 지정한 url에 put Method 방식으로 data의 Submit Dictionary를
-    전달해서 DB를 수정하고, 그 결과(Response)를 Return 합니다.
-
-    url: put 메서드를 전달 할 웹 페이지 URL(String)
-    data: 웹 페이지에 put Method 방식으로 전달할 submit data(Dictionary)
-    cookie: 웹 페이지에 전달할 쿠기 정보(String)
-    headers: HTTP Request header 부분에 포함할 parameters(Dictionary)
-    retries: 서버 에러 시 접속 재시도(getDownload 함수 재호출) 횟수(integer)
-    '''
-    resp = None
-
-    try:
-        resp = requests.put(url, data=data, cookies=cookie, headers=headers)
-        resp.raise_for_status()
-    except HTTPError as e:
-        if 500 <= resp.status_code < 600 and retries > 0:
-            print("Retries: {0}".format(base_retries - retries + 1))
-            return putDownload(url, data, cookie, headers, retries - 1)
-        else:
-            print("HTTPError:[{}]:{}, {}".format(resp.status_code, resp.reason,
-                                                 resp.headers))
-    except ConnectionError as e:
-        print("ConnectionError:{}".format(e))
-    except RequestException as e:
-        print("UnexpectedError:{}".format(e))
-
-    return resp
