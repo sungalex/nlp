25042bd581b3d8c96de18a76d5631edce2353d95
sungsalex <cloudallera@gmail.com>
2019-03-13 13:53:50 +0900

    ppomppu 모듈 작성 및 테스트(TBD)
    Signed-off-by: sungsalex <cloudallera@gmail.com>

--- functions/ppomppu.py
+++ functions/ppomppu.py
@@ -10,8 +10,6 @@ from datetime import datetime
 import os
 import sys
 
-url = "http://www.ppomppu.co.kr/zboard/zboard.php"
-
 
 class Ppomppu():
     '''
@@ -19,43 +17,128 @@ class Ppomppu():
     함수들을 포함하고 있습니다.
     '''
 
-    def __init__(self):
-        self.param = {"id": "ppomppu"}
-        self.html = getDownload(url, self.param)
+    def __init__(self, category=None):
+        '''
+        Ppomppu 객체를 생성합니다.
+        
+        객체 생성 시 category 값을 전달하면, 해당 category 게시글만 List 됩니다.
+        category를 지정하지 않으면 전체 게시글이 List 됩니다.
+        
+        category 값은 아래와 같습니다.
+            1:기타
+            4~6:컴퓨터,디니털,먹거리
+            8~13:서적,가전/가구,육아,카메라,의류/잡화,화장품
+            15:등산/캠핑
+        '''
+        self.url = "http://www.ppomppu.co.kr/zboard/zboard.php"
+        self.param = {"id": "ppomppu", "category": category}
+        self.html = getDownload(self.url, self.param)
         self.bs = BeautifulSoup(self.html.text, "html.parser")
+        self.intStrings = self.getIntStrings()
 
-    def getNumber(self):
-        pass
+    def getIntStrings(self):
+        '''
+        "td.eng.list_vspace" 클래스명으로 나타나는 integer String형 자료
+        3개(3개 열)의 값을 반환 합니다.
+        
+        이 3개의 열을 구분할 만한 ID/Class 정보가 없기 때문에 한꺼번에 추출 후
+        순서에 따하 해당 컬럼 자료로 분류해서 사용 합니다.
+        첫번째 자료는 "번호", 두번째 자료는 "추천", 세번째 자료는 "조회수"를
+        의미 합니다. getNumbers(), getLikeCounts(), getQueryCounts() 함수에서
+        이 값을 사용합니다.
 
-    def getCategory(self):
-        pass
+        이 함수는 객체 생성 시 호출됩니다.
+        '''
+        intStrings = []
 
-    def getWriter(self):
-        pass
+        for tag in self.bs.select("tr > td.eng.list_vspace"):
+            if not tag.has_attr("title"):
+                intStrings.append(tag.text.strip())
 
-    def getTitle(self):
-        title = []
+        return intStrings
+
+    def getNumbers(self):
+        '''
+        게시글 번호를 반환 합니다.
+        '''
+        numbers = []
+
+        for intString in self.intStrings:
+            if self.intStrings.index(intString) % 3 == 0:
+                numbers.append(intString)
+
+        return numbers
+
+    def getCategorys(self):
+        '''
+        게시글 별 분류 카테고리를 반환 합니다.
+
+        '''
+        categorys = []
+
+        for tag in self.bs.select("td.han4.list_vspace"):
+            categorys.append(tag.text.strip())
+
+        return categorys
+
+    def getWriters(self):
+        '''
+        게시글 별 글쓴이 필명을 반환 합니다.
+        '''
+        writers = []
+
+        for tag in self.bs.select("div.list_name"):
+            if not tag.find_parent().find_parent()["class"][0] == "list_notice":
+                writer = tag.find("a").text.strip()
+                if len(writer) == 0:
+                    if tag.find("img").has_attr("alt"):
+                        writers.append(tag.find("img")["alt"])
+                    else:
+                        writers.append(None)
+                else:
+                    writers.append(writer)
+
+        return writers
+
+    def getTitles(self):
+        '''
+        게시글 별 제목을 반환 합니다.
+        '''
+        titles = []
         for tag in self.bs.select("table font.list_title"):
-            title.append(tag.text.strip())
-        return title
+            titles.append(tag.text.strip())
+        return titles
 
-    def getLink(self):
-        link = []
+    def getLinks(self):
+        '''
+        게시글 별 본문 링크를 반환 합니다.
+        '''
+        links = []
         for tag in self.bs.select("table font.list_title"):
-            link.append(urljoin(url, tag.find_parent()["href"]))
-        return link
+            links.append(urljoin(self.url, tag.find_parent()["href"]))
+        return links
 
-    def getImageSource(self):
-        imgList = []
+    def getImageLinks(self):
+        '''
+        게시글 별 섬네일 이미지 링크를 반환 합니다.
+        '''
+        imgLinks = []
 
         for tag in self.bs.select("table img.thumb_border"):
-            imgList.append(urljoin(url, tag["src"]))
+            imgLinks.append(urljoin(self.url, tag["src"]))
 
-        return imgList
+        return imgLinks
 
-    def downloadImage(self):
-        imgList = self.getImageSource()
+    def getImages(self):
+        '''
+        게시글 별 섬네일 이미지를 다운로드 하고, 저장된 파일 위치를 반환 합니다.
 
+        이미지 저장 위치는 "images/ppomppu/" 폴더 아래에 현재
+        "년-월-일_시간_분" 폴더를 만들고, 그 아래에 저장 됩니다.
+        '''
+        imgLinks = self.getImageLinks()
+
+        # image/ppomppu 아래에 현재 시간("년-월-일_시간_분")으로 폴더 만들기
         now = datetime.now().isoformat()
         day_hour_minute = now[:10] + "_" + now[11:13] + "-" + now[14:16]
         path = "images/ppompu/" + day_hour_minute
@@ -63,12 +146,11 @@ class Ppomppu():
             if not os.path.exists(path):
                 os.makedirs(path)
         except:
-            raise os.OSError(
-                "(%s) 디렉토리를 만들 수 없습니다!" % (path))
+            raise os.OSError("(%s) 디렉토리를 만들 수 없습니다!" % (path))
 
-        fileList = []
+        images = []
 
-        for src in imgList:
+        for src in imgLinks:
             img = getDownload(src)
             imgName = src.split("/")[-1].split("?")[0]
 
@@ -76,30 +158,113 @@ class Ppomppu():
 
             with open(fileName, "wb") as f:
                 f.write(img.content)
-                fileList.append(fileName)
+                images.append(fileName)
 
-        return fileList
+        return images
 
-    def getReplyCount(self):
-        pass
+    def getReplyCounts(self):
+        '''
+        게시글 별 댓글 수를 반환 합니다.
+        '''
+        replyCounts = []
 
-    def getWriteDate(self):
-        pass
+        for tag in self.bs.select("table font.list_title"):
+            parent = tag.find_parent()
+            if not parent.find_next_sibling():
+                replyCounts.append(0)
+            else:
+                replyCount = parent.find_next_sibling().span.text.strip()
+                replyCounts.append(int(replyCount))
 
-    def getLikeCount(self):
-        pass
+        return replyCounts
 
-    def getQueryCount(self):
-        pass
+    def getWriteDates(self):
+        '''
+        게시글 별 작성 일시를 반환 합니다.
+        '''
+        writeDates = []
 
-    def getReplyContent(self):
-        pass
+        for tag in self.bs.select("td.eng.list_vspace"):
+            if tag.has_attr("title"):
+                writeDates.append(tag["title"])
+
+        return writeDates
+
+    def getLikeCounts(self):
+        '''
+        게시글 별 추천 회수를 반환 합니다.
+        '''
+        likes = []
+
+        for intString in self.intStrings:
+            if self.intStrings.index(intString) % 3 == 1:
+                like = intString.split("-")[0].strip()
+                if len(like) == 0:
+                    likes.append(0)
+                else:
+                    likes.append(int(like))
+
+        return likes
+
+    def getQueryCounts(self):
+        '''
+        게시글별 조회 수를 반환 합니다.
+        '''
+        queryCounts = []
+
+        for intString in self.intStrings:
+            if self.intStrings.index(intString) % 3 == 2:
+                if len(intString) == 0:
+                    queryCounts.append(0)
+                else:
+                    queryCounts.append(int(intString))
+
+        return queryCounts
+
+    def getContentBodys(self):
+        '''
+        게시글 별 본문 내용을 반환 합니다.
+        
+        //To-Do
+        '''
+        return None
 
     def getComments(self):
-        pass
+        '''
+        게시글 별 Comments를 반환 합니다.
 
-    def getPpomppuList(self):
-        pass
+        //To-Do
+        '''
+        return None
+
+    def getPpomppuBbs(self):
+        '''
+        뽐뿌게시판 전체 column을 리스트로 반환 합니다.
+        
+        번호, 분류, 글쓴이, 제목, 본문 링크, 이미지 링크, 이미지
+        파일, 댓글 수, 작성일시, 추천 수, 조회 수, 게시글 본문, 댓글을
+        List의 List로 반환 합니다.
+        '''
+        numbers = self.getNumbers()
+        categorys = self.getCategorys()
+        writers = self.getWriters()
+        titles = self.getTitles()
+        links = self.getLinks()
+        imageLinks = self.getImageLinks()
+        images = self.getImages()
+        replyCounts = self.getReplyCounts()
+        writeDates = self.getWriteDates()
+        likes = self.getLikeCounts()
+        queryCounts = self.getQueryCounts()
+        contents = self.getContentBodys()
+        comments = self.getComments()
+
+        ppList = [
+            numbers, categorys, writers, titles, links, imageLinks, images,
+            replyCounts, writeDates, likes, queryCounts, contents, comments
+        ]
+
+        return ppList
 
 
 class PpomppuFreeboard():
@@ -109,41 +274,45 @@ class PpomppuFreeboard():
     '''
 
     def __init__(self):
+        self.url = "http://www.ppomppu.co.kr/zboard/zboard.php"
         self.param = {"id": "freeboard"}
-        self.html = getDownload(url, self.param)
+        self.html = getDownload(self.url, self.param)
         self.bs = BeautifulSoup(self.html.text, "html.parser")
 
-    def getNumber(self):
+    def getNumbers(self):
         pass
 
-    def getCategory(self):
+    def getCategorys(self):
         pass
 
-    def getWriter(self):
+    def getWriters(self):
         pass
 
-    def getTitle(self):
+    def getTitles(self):
         pass
 
-    def getUrl(self):
+    def getUrls(self):
         pass
 
-    def getImage(self):
+    def getImageLinks(self):
         pass
 
-    def getReplyCount(self):
+    def getImages(self):
         pass
 
-    def getWriteDate(self):
+    def getReplyCounts(self):
         pass
 
-    def getLikeCount(self):
+    def getWriteDates(self):
         pass
 
-    def getQueryCount(self):
+    def getLikeCounts(self):
         pass
 
-    def getReplyContent(self):
+    def getQueryCounts(self):
+        pass
+
+    def getReplyContents(self):
         pass
 
     def getComments(self):
